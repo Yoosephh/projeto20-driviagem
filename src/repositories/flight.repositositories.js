534ac48392ce44995d00058b3c6421fffcfe48c6
José Eduardo <105430849+Yoosephh@users.noreply.github.com>
2023-09-11 15:33:52 -0300

    ::date

--- src/repositories/flight.repositositories.js
+++ src/repositories/flight.repositositories.js
@@ -1,7 +1,7 @@
 import db from "../database/database.connection.js";
 
 async function registerFlight(origin, destination, date) {
-  return await db.query(`INSERT INTO flights (origin, destination, date) VALUES ($1, $2, $3)`, [origin, destination, date])
+  return await db.query(`INSERT INTO flights (origin, destination, date) VALUES ($1, $2, $3::date)`, [origin, destination, date])
 }
 
 async function registerTravel(passengerId, flightId) {
